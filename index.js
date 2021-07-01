18b0b78f4d24ed20b47a3b01e426090d216f61ea
KristineZegnere <84313893+KristineZegnere@users.noreply.github.com>
2021-07-01 17:21:22 +0300

    Saving..

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 
+window.onload = function(){
+    bingoBoardCells()
+  
+}
 
-window.onload = {
-   
+const bingoBoardCells = function() {
+    let newCellsPlace = document.getElementById('cells-for-numbers')
+
+    for(let cellNum = 0; cellNum <= 76; cellNum++) {
+        let newCell = document.createElement('div')
+        newCell.innerText = cellNum
+        newCell.classList.add('one-cell')
+     //   newCell.onclick = selectNumber
+        newCellsPlace.appendChild(newCell)
+    
+} 
+}
+
+    const selectNumber = function() {
+        
+    }
+
+const randomNumber = function() {
+    let oneRandomNumber = Math.floor(Math.random() * 77)
+
+    if(oneRandomNumber === newCell) {
+        oneRandomNumber.style.color = 'yellow'
+    }
 }
